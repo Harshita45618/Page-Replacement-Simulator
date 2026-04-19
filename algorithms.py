331c5bc7aa93c89115703ff9dda767478647b56b
Harshita45618 <kharshita9a17@gmail.com>
2026-04-19 10:10:51 +0530

    Implemented FIFO page replacement algorithm
    Initial implementation of FIFO algorithm for page replacement simulation

---
+++ algorithms.py
@@ -0,0 +1,21 @@
+def fifo(pages, capacity):
+    frames, faults = [], 0
+    history = []
+
+    for page in pages:
+        if page not in frames:
+            if len(frames) < capacity:
+                frames.append(page)
+            else:
+                frames.pop(0)
+                frames.append(page)
+            faults += 1
+            status = "Fault"
+        else:
+            status = "Hit"
+
+        history.append((page, frames.copy(), status))
+
+    return faults, history
+
+
