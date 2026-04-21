8d0aa84a07b2733f2eac47a5aecbfc0adda9b605
Harshita45618 <kharshita9a17@gmail.com>
2026-04-21 09:31:19 +0530

    Added LRU page replacement algorithm

--- algorithms.py
+++ algorithms.py
@@ -18,4 +18,26 @@ def fifo(pages, capacity):
 
     return faults, history
 
+def lru(pages, capacity):
+    frames, faults = [], 0
+    history = []
+
+    for page in pages:
+        if page not in frames:
+            if len(frames) < capacity:
+                frames.append(page)
+            else:
+                frames.pop(0)
+                frames.append(page)
+            faults += 1
+            status = "Fault"
+        else:
+            frames.remove(page)
+            frames.append(page)
+            status = "Hit"
+
+        history.append((page, frames.copy(), status))
+
+    return faults, history
+
 
