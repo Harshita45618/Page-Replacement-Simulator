85c2fdc7e63a71d04c0650dd5cc820b7a48a7b2e
Harshita45618 <kharshita9a17@gmail.com>
2026-04-24 11:22:29 +0530

    Added MRU (Most Recently Used) page replacement algorithm and integrated it into simulator UI and comparison module

--- algorithms.py
+++ algorithms.py
@@ -77,3 +77,29 @@ def optimal(pages, capacity):
         history.append((current_page, frames.copy(), status))
 
     return faults, history
+
+
+def mru(pages, capacity):
+    frames = []
+    faults = 0
+    history = []
+
+    for page in pages:
+        if page not in frames:
+            if len(frames) < capacity:
+                frames.append(page)
+            else:
+                # Remove most recently used page (last element)
+                frames.pop(-1)
+                frames.append(page)
+            faults += 1
+            status = "Fault"
+        else:
+            # Move page to most recent position
+            frames.remove(page)
+            frames.append(page)
+            status = "Hit"
+
+        history.append((page, frames.copy(), status))
+
+    return faults, history
